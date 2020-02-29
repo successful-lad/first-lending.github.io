efc864550f119a364e4260de9af4a585ac1f6180
egorglebenko <blessedonwebdev@gmail.com>
2020-02-29 10:41:46 +0200

    ended main screen

--- src/components/HeaderMenu/index.js
+++ src/components/HeaderMenu/index.js
@@ -1,36 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from 'react-router-dom';
+import classNames from 'classnames';
 import logoIcon from '../../assets/img/logo.svg';
 import menuIcon from '../../assets/img/menu-icon.svg'
 
 import './style.scss';
 
-const HeaderMenu = () => (
-  <div className='headerMenu'>
-    <div className='headerMenu__logo'>
-      <img src={logoIcon} alt="logo"/>
-      APP-KIT
-    </div>
-    <nav className='headerMenu__menu'>
-      <Link className='headerMenu__menu__item'>About</Link>
-      <Link className='headerMenu__menu__item'>Testimonials</Link>
-      <Link className='headerMenu__menu__item'>Features</Link>
-      <Link className='headerMenu__menu__item'>Team</Link>
-      <Link className='headerMenu__menu__item'>Pricing</Link>
-      <Link className='headerMenu__menu__item'>Contact</Link>
+const HeaderMenu = () =>{
 
-      <div
-        className='headerMenu__menu__burgerMenu'
-        onClick={()=>{alert('itsWork')}}
-      >
-        <img
-          src={menuIcon}
-          alt="burger menu"
-        />
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const menuClasses = classNames({
+    'headerMenu__menu': true,
+    'headerMenu__menu_side' : menuIsOpen,
+  });
+
+  const menuItemClasses = classNames({
+    'headerMenu__menu__item': true,
+    'headerMenu__menu_side__item': menuIsOpen,
+  });
+
+  console.log(menuIsOpen);
+
+  return(
+    <div className='headerMenu'>
+      <div className='headerMenu__logo'>
+        <img src={logoIcon} alt="logo"/>
+        APP-KIT
       </div>
-    </nav>
+      <nav className={menuClasses}>
+        <Link to={'/'} className={menuItemClasses}>About</Link>
+        <Link to={'/'} className={menuItemClasses}>Testimonials</Link>
+        <Link to={'/'} className={menuItemClasses}>Features</Link>
+        <Link to={'/'} className={menuItemClasses}>Team</Link>
+        <Link to={'/'} className={menuItemClasses}>Pricing</Link>
+        <Link to={'/'} className={menuItemClasses}>Contact</Link>
+        <div
+          className='headerMenu__menu__burgerMenu'
+          onClick={() => setMenuIsOpen(value => !value)}
+        >
+          <img
+            src={menuIcon}
+            alt="burger menu"
+          />
+        </div>
+      </nav>
 
-  </div>
-);
+    </div>
+  );
+};
 
 export default HeaderMenu;
